0d70d0bf8da823adcb099cb62bbeb0d52dbb0a88
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2021-09-08 23:36:17 -0400

    basic form validation

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {useState, usestate} from 'react';
+
+import Form from './components/Form'
 
 function App() {
+  const [team, setTeam] = useState([])
+
   return (
     <div className="App">
-
+      <Form />
     </div>
   );
 }
---
+++ app/src/components/Form.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+export default function Form() {
+
+  // Schema ===
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required(),
+    email: yup
+      .string()
+      .email()
+      .required(),
+    role: yup
+      .string()
+      .oneOf(["student", "team lead", "instructor"])
+      .required(),
+  })
+
+  // useForm Hook ===
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  // handleSubmit
+  const onSubmit = data => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        Name:
+        <input {...register("name")}/>
+      </label>
+      <p>{errors.name?.message}</p>
+      <label>
+        Email:
+        <input {...register("email")}/>
+      </label>
+      <p>{errors.email?.message}</p>
+      <label>
+        Role:
+        <select {...register("role")}>
+          <option selected value="none">None</option>
+          <option value="student">Student</option>
+          <option value="team lead">Team Lead</option>
+          <option value="instructor">Instructor</option>
+        </select>
+      </label>
+      <p>{errors.role?.message}</p>
+      <input type="submit"/>
+    </form>
+  )
+}
